5e29529de4ca548b84a3c21b74706bac41ba1065
Илья Халяпин <962426@mail.ru>
2017-05-31 23:36:02 +0300

    Реализована армия

---
+++ StackGame/Army/Army.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using StackGame.Units;
+
+namespace StackGame.Army
+{
+    /// <summary>
+    /// Армия
+    /// </summary>
+    public class Army : IArmy
+    {
+        #region Свойства
+
+        /// <summary>
+        /// Единицы армии
+        /// </summary>
+        public List<IUnit> Units { get; private set; }
+        /// <summary>
+        /// Название армии
+        /// </summary>
+        private string name;
+
+		#endregion
+
+		#region Инициализация
+
+		public Army(string name)
+		{
+            Units = CreateArmy(1000);
+            this.name = name;
+		}
+
+		#endregion
+
+		#region Методы
+
+		/// <summary>
+		/// Создать армию
+		/// </summary>
+        List<IUnit> CreateArmy(int cost) 
+        {
+            return new List<IUnit>();
+        }
+
+		/// <summary>
+		/// Преобразовать в строку
+		/// </summary>
+        public override string ToString() {
+            string army = $"Армия: { name }\n";
+            foreach (var unit in Units) 
+            {
+                army += unit.ToString() + "\n";
+            }
+
+            return army;
+        }
+
+		#endregion
+	}
+}
---
+++ StackGame/Army/IArmy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using StackGame.Units;
+
+namespace StackGame.Army
+{
+    /// <summary>
+    /// Интерфейс армии
+    /// </summary>
+    public interface IArmy
+    {
+		#region Свойства
+
+        /// <summary>
+        /// Единицы армии
+        /// </summary>
+        List<IUnit> Units { get; }
+
+		#endregion
+
+		#region Методы
+
+		/// <summary>
+		/// Преобразовать в строку
+		/// </summary>
+		string ToString();
+
+		#endregion
+	}
+}
