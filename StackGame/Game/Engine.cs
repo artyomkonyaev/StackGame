e341a10cc36881c263aee55ac260f170105e2f1d
Илья Халяпин <962426@mail.ru>
2017-05-31 21:14:25 +0300

    Реализован engine игры

---
+++ StackGame/Game/Engine.cs
@@ -0,0 +1,41 @@
+﻿namespace StackGame.Game
+{
+    /// <summary>
+    /// Движок игры
+    /// </summary>
+    public class Engine
+    {
+        #region Свойства
+
+        /// <summary>
+        /// Экземпляр класса
+        /// </summary>
+        private static Engine instance;
+
+		#endregion
+
+		#region Инициализация
+
+		private Engine()
+        { }
+
+		#endregion
+
+		#region Методы
+
+        /// <summary>
+        /// Получить экземпляр класса
+        /// </summary>
+        public static Engine GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new Engine();
+            }
+
+            return instance;
+        }
+
+		#endregion
+	}
+}
