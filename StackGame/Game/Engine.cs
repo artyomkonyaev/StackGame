eb82bb2f2ac778fa647d9aaacb9ab9dceb67b1d2
Илья Халяпин <962426@mail.ru>
2017-06-01 23:17:15 +0300

    В Engine добавлены армии, реализован конструктор, реализован метод NextStep

--- StackGame/Game/Engine.cs
+++ StackGame/Game/Engine.cs
@@ -1,6 +1,12 @@
-﻿namespace StackGame.Game
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using StackGame.Units;
+using StackGame.Army;
+
+namespace StackGame.Game
 {
-    /// <summary>
+	/// <summary>
     /// Движок игры
     /// </summary>
     public class Engine
@@ -12,16 +18,28 @@
         /// </summary>
         private static Engine instance;
 
-		#endregion
+        /// <summary>
+        /// Первая армия
+        /// </summary>
+        private IArmy firstArmy { get; set; }
+        /// <summary>
+        /// Вторая армия
+        /// </summary>
+        private IArmy secondArmy { get; set; }
 
-		#region Инициализация
+        #endregion
 
-		private Engine()
-        { }
+        #region Инициализация
 
-		#endregion
+        private Engine()
+        {
+            firstArmy = new Army.Army("Белая");
+            secondArmy = new Army.Army("Черная");
+        }
 
-		#region Методы
+        #endregion
+
+        #region Методы
 
         /// <summary>
         /// Получить экземпляр класса
@@ -34,6 +52,62 @@
             }
 
             return instance;
+        }
+
+        /// <summary>
+        /// Следующий ход
+        /// </summary>
+        public bool NextStep() {
+            if (firstArmy.IsAllDead || secondArmy.IsAllDead) 
+            {
+                return false;
+            }
+
+            // Создаем массив противников
+            var _opponents = new Opponents(firstArmy.Units[0], secondArmy.Units[0]);
+            var queue = new List<Opponents>
+            {
+                _opponents,
+                _opponents.Reverse()
+            };
+
+            var random = new Random();
+            queue = queue.OrderBy(opponents => random.Next()).ToList();
+
+            // Бой
+            foreach (var opponents in queue)
+            {
+                Hit(opponents.FirstArmyUnit, opponents.SecondArmyUnit);
+            }
+
+            // Удаление убитых
+            RemoveDeadUnits(firstArmy);
+            RemoveDeadUnits(secondArmy);
+
+            return true;
+        }
+
+        /// <summary>
+        /// Атаковать противника
+        /// </summary>
+        private void Hit(IUnit first, IUnit second)
+        {
+            if (first.IsAlive)
+            {
+                second.GetDamage(first.Strength);
+            }
+        }
+
+        /// <summary>
+        /// Удалить мертвые единицы армии
+        /// </summary>
+        private void RemoveDeadUnits(IArmy army)
+        {
+            var dead = army.Units.Where(unit => !unit.IsAlive).ToList();
+            foreach (var unitToBeDeleted in dead)
+            {
+                army.Units.Remove(unitToBeDeleted);
+            }
         }
 
 		#endregion
---
+++ StackGame/Game/Opponents.cs
@@ -0,0 +1,46 @@
+﻿using StackGame.Units;
+
+namespace StackGame.Game
+{
+    /// <summary>
+    /// Противники
+    /// </summary>
+    public struct Opponents
+    {
+        #region Свойства
+
+        /// <summary>
+        /// Единица первой армии
+        /// </summary>
+        public IUnit FirstArmyUnit { get; private set; }
+        /// <summary>
+        /// Единица второй армии
+        /// </summary>
+        public IUnit SecondArmyUnit { get; private set; }
+
+		#endregion
+
+		#region Инициализация
+
+        public Opponents(IUnit firstArmyUnit, IUnit secondArmyUnit) 
+        {
+            FirstArmyUnit = firstArmyUnit;
+            SecondArmyUnit = secondArmyUnit;
+        }
+
+		#endregion
+
+		#region Методы
+
+		/// <summary>
+		/// Поменять единицы армий местами
+		/// </summary>
+		public Opponents Reverse() 
+        {
+            var opponents = new Opponents(SecondArmyUnit, FirstArmyUnit);
+            return opponents;
+        }
+
+		#endregion
+	}
+}
