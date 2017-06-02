09789b1e4a1af84f3b6b04b3429a8a6f4b684c00
Илья Халяпин <962426@mail.ru>
2017-06-03 01:54:47 +0300

    Реализована специальная возможность для легкого пехотинца

--- StackGame/Units/LightUnit.cs
+++ StackGame/Units/LightUnit.cs
@@ -1,12 +1,23 @@
-﻿using StackGame.Units.Abilities;
+﻿using System;
+using System.Linq;
+using StackGame.Army;
+using StackGame.Units.Abilities;
+using StackGame.Units.Improvements;
 
 namespace StackGame.Units
 {
     /// <summary>
     /// Легкий пехотинец
     /// </summary>
-    public class LightUnit : Unit, IHealable, IClonable
+    public class LightUnit : Unit, IHealable, IClonable, ISpecialAbility
     {
+		#region Свойства
+
+		public int Range { get; } = 1;
+		public int Power { get; } = 0;
+
+		#endregion
+
 		#region Инициализация
 
 		public LightUnit() : base(100, 15)
@@ -28,6 +39,34 @@ namespace StackGame.Units
 		public IUnit Clone()
 		{
 			return (IUnit)MemberwiseClone();
+		}
+
+		public void DoSpecialAction(IArmy targetArmy, int unitPosition)
+		{
+			var random = new Random();
+			var chance = random.Next(100 / 50) == 0;
+
+            if (chance && targetArmy.Units[unitPosition] is IImprovable improvableUnit)
+			{
+                var unitImproveType = typeof(UnitImprove<>);
+                var types = unitImproveType.Assembly.GetTypes().Where(type => type.BaseType != null && type.BaseType.IsGenericType).Where(type => type.BaseType.GetGenericTypeDefinition() == unitImproveType.GetGenericTypeDefinition()).ToList();
+
+                do
+                {
+                    var type = types[random.Next(types.Count)];
+                    if (improvableUnit.CanImprove(type)) 
+                    {
+                        var unitImprove = type.MakeGenericType(improvableUnit.GetType());
+                        var improvementUnit = (IUnit)Activator.CreateInstance(unitImprove, improvableUnit);
+
+                        targetArmy.Units[unitPosition] = improvementUnit;
+
+                        break;
+                    }
+
+                    types.Remove(type);
+                } while (types.Count != 0);
+			}
 		}
 
         public override string ToString()
