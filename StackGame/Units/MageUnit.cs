59c64af51c053c91cd714507d222ca7d44fe2035
Илья Халяпин <962426@mail.ru>
2017-06-02 16:22:24 +0300

    Для мага реализована специальная возможность

--- StackGame/Units/MageUnit.cs
+++ StackGame/Units/MageUnit.cs
@@ -1,10 +1,20 @@
-﻿namespace StackGame.Units
+﻿using StackGame.Army;
+using StackGame.Units.Abilities;
+
+namespace StackGame.Units
 {
     /// <summary>
     /// Маг
     /// </summary>
-    public class MageUnit : Unit
+    public class MageUnit : Unit, ISpecialAbility
     {
+		#region Свойства
+
+		public int Range { get; } = 3;
+		public int Power { get; } = 0;
+
+		#endregion
+
 		#region Инициализация
 
 		public MageUnit() : base(100, 5)
@@ -13,6 +23,15 @@
 		#endregion
 
 		#region Методы
+
+		public void DoSpecialAction(IArmy targetArmy, IUnit targetUnit)
+		{
+            if (targetUnit is IClonable clonableUnit)
+            {
+                var unit = clonableUnit.Clone();
+                targetArmy.Units.Add(unit);
+            }
+		}
 
 		public override string ToString()
 		{
