65fc06206035bc342073a6fe16543678fc3ac3be
Илья Халяпин <962426@mail.ru>
2017-06-02 15:00:03 +0300

    Реализован интерфейс ISpecialAbility

---
+++ StackGame/Units/Abilities/ISpecialAbility.cs
@@ -0,0 +1,32 @@
+﻿using StackGame.Army;
+
+namespace StackGame.Units.Abilities
+{
+    /// <summary>
+    /// Интерфейс специальных возможностей
+    /// </summary>
+    public interface ISpecialAbility
+    {
+		#region Свойства
+
+		/// <summary>
+		/// Дальность (измеряется в единицах армии)
+		/// </summary>
+		int Range { get; }
+        /// <summary>
+        /// Мощность
+        /// </summary>
+        int Power { get; }
+
+        #endregion
+
+        #region Методы
+
+        /// <summary>
+        /// Применить специальную возможность
+        /// </summary>
+        void DoSpecialAction(IArmy targetArmy, IUnit targetUnit);
+
+		#endregion
+	}
+}
