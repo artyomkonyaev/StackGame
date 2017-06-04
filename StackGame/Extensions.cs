36838e8ad355e7f03bddd7a5c729be9109fadc2d
Илья Халяпин <962426@mail.ru>
2017-06-04 16:57:05 +0300

    Реализовано расширение IEnumerable, реализующее удобный способ рандомно перемешать IEnumerable

---
+++ StackGame/Extensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StackGame
+{
+    /// <summary>
+    /// Расширения
+    /// </summary>
+    public static class Extensions
+    {
+        /// <summary>
+        /// Перемешать
+        /// </summary>
+		public static IEnumerable<T> Randomize<T>(this IEnumerable<T> source)
+		{
+			Random rnd = new Random();
+			return source.OrderBy(item => rnd.Next());
+		}
+    }
+}
